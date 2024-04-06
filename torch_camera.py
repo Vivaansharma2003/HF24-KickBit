4b8eefba5d8e979cf55e08382158b880511b8a7d
Aayushi Padia <aayushipadia1210@gmail.com>
2024-04-07 02:27:54 +0530

    Location+SOS in Accident Detection

--- torch_camera.py
+++ torch_camera.py
@@ -1,5 +1,7 @@
 from torch_detection import ReturnLoadedCNN
 import cv2
+import geocoder
+import requests
 
 model = ReturnLoadedCNN(path_to_weights='models/crash_cnn.pth')
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -15,6 +17,29 @@ def start_application():
 
                 cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
                 cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
+
+                # Don't actually want to send an SOS to the government since it is testing, so avoiding this part of the code.
+                '''g = geocoder.ip('me')
+                print(g.latlng)
+
+                api_url = "https://emergency-api.example.in/report"  #Example - Haven't added the actual government emergency API
+
+                latitude = g.latlng[0]  
+                longitude = g.latlng[1]
+
+                data = {
+                        "message": "SOS! Accident detected",
+                        "latitude": latitude,
+                        "longitude": longitude
+                        }
+
+                # Send POST request with accident data
+                response = requests.post(api_url, json=data)
+
+                if response.status_code == 200:
+                    print("SOS message sent successfully:", response.text)
+                else:
+                    print("Error sending SOS message:", response.status_code, response.text)'''
 
             if cv2.waitKey(33) & 0xFF == ord('q'):
                 return
